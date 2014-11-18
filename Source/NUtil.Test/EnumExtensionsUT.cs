b4ead68293c0f6bab5d5cf20e1ed8ba66f1b90b5
Reonekot <toke@noer.it>
2014-11-18 16:57:52 +0100

    Added EnumExtensions for fast cached enum lookup

---
+++ Source/NUtil.Test/EnumExtensionsUT.cs
@@ -0,0 +1,50 @@
+﻿namespace NUtil.Test
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class EnumExtensionsUT
+    {
+        enum X
+        {
+            x1, x2, x3, x4,
+            X5, X6, X7, X8
+        }
+
+        [TestMethod]
+        public void EnumAsStringMustReturnCorrectValue()
+        {
+            var enumVal = X.X5;
+
+            var myX = enumVal.AsString();
+
+            Assert.AreEqual("X5", myX);
+        }
+
+        [TestMethod]
+        public void StringAsEnumMustRoundTrip()
+        {
+            var enumVal = X.X5;
+            var myX = enumVal.AsString();
+
+            X valAsEnum = myX.AsEnum<X>();
+
+            Assert.IsTrue(valAsEnum == enumVal);
+            Assert.AreEqual("X5", myX);
+        }
+
+        [TestMethod]
+        public void StringAsEnumShouldIgnoreCase()
+        {
+            var enumVal = X.X5;
+            var myX = enumVal.AsString();
+            myX = myX.ToLowerInvariant();
+
+            X valAsEnum = myX.AsEnum<X>();
+
+            Assert.AreEqual("x5", myX);
+            Assert.IsTrue(valAsEnum == enumVal);
+        }
+    }
+}
---
+++ Source/NUtil/EnumExtensions.cs
@@ -0,0 +1,62 @@
+﻿namespace System
+{
+    using System.Collections.Generic;
+
+    public static class EnumExtensions
+    {
+        public static string AsString<TEnum>(this TEnum enumValue) where TEnum : struct, IConvertible
+        {
+            return EnumCache<TEnum>.Get(enumValue);
+        }
+
+        public static TEnum AsEnum<TEnum>(this string enumName) where TEnum : struct, IConvertible
+        {
+            return EnumCache<TEnum>.Get(enumName);
+        }
+
+        private static class EnumCache<TEnum>
+        {
+            private static Dictionary<TEnum, string> EnumToStringValues = new Dictionary<TEnum, string>();
+            private static Dictionary<string, TEnum> StringToEnumValues = new Dictionary<string, TEnum>();
+
+            static EnumCache()
+            {
+                var t = typeof(TEnum);
+                var values = (TEnum[])Enum.GetValues(t);
+                var names = Enum.GetNames(t);
+
+                for (var i = 0; i < values.Length; ++i)
+                {
+                    EnumToStringValues.Add(values[i], names[i]);
+                    StringToEnumValues.Add(names[i], values[i]);
+
+                    var lowerCaseFallback = names[i].ToLowerInvariant();
+                    if (lowerCaseFallback != names[i])
+                        StringToEnumValues.Add(lowerCaseFallback, values[i]);
+                }
+            }
+
+            internal static string Get(TEnum enumValue)
+            {
+                return EnumToStringValues[enumValue];
+            }
+
+            internal static TEnum Get(string enumName)
+            {
+                TEnum value;
+                var success = StringToEnumValues.TryGetValue(enumName, out value);
+                if (!success)
+                {
+                    // Try get lower case version
+                    success = StringToEnumValues.TryGetValue(enumName.ToLowerInvariant(), out value);
+                    if (!success)
+                    {
+                        throw new ArgumentOutOfRangeException("enumName not found: " + enumName);
+                    }
+                }
+
+                return value;
+            }
+        }
+    }
+}
